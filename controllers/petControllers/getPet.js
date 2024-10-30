141429cfbca5ffe17d58d4c95161b9585147e034
TIAGO DO CARMO CAVALCANTI <TIAGO1462@IOT>
2024-10-30 11:27:31 -0300

    working on controllers pattern

--- controllers/petControllers/getPet.js
+++ controllers/petControllers/getPet.js
@@ -1,8 +1,4 @@
-const { PersonRegister, Address, Contact, Role, Pet, Patient, Cage, Treatment, Medication, PatientsDailyChart } = require("../db/models/modelSequelize")
-
-function helloWorld(req, res) {
-    res.send("Hello World!");
-};
+const { PersonRegister, Address, Contact, Role, Pet, Patient, Cage, Treatment, Medication, PatientsDailyChart } = require("../../db/models/modelSequelize")
 
 async function getAllPets(req, res){
     try {
@@ -11,7 +7,7 @@ async function getAllPets(req, res){
     } catch (error) {
       res.status(500).json({ error: 'Erro ao buscar os pets.' + error });
     }
-  }
+  };
 
   async function getPetById(req, res){
     try {
@@ -37,7 +33,7 @@ async function getAllPets(req, res){
     } catch (error) {
       res.status(500).json({ error: 'Erro ao buscar pets pelo nome.' + error});
     }
-  }
+  };
 
   async function getMostRecentPet(req, res){
     try {
@@ -52,7 +48,7 @@ async function getAllPets(req, res){
     } catch (error) {
       res.status(500).json({ error: 'Erro ao buscar o pet mais recente.'  + error});
     }
-  }
+  };
   
   async function getPetByTutorName(req, res){
     const { tutorName } = req.params;
@@ -82,84 +78,12 @@ async function getAllPets(req, res){
     } catch (error) {
       res.status(500).json({ error: 'Erro ao buscar pets pelo nome do tutor.' + error });
     }
-  }
+  };
 
-  async function createPet (req, res){
-    const { name, species, breed, color, weight, sex, age, photo, behavior, neurologicalDisorders, notes } = req.body;
-  
-    try {
-      const newPet = await Pet.create({
-        name,
-        species,
-        breed,
-        color,
-        weight,
-        sex,
-        age,
-        photo,
-        behavior,
-        neurologicalDisorders,
-        notes
-      });
-      res.status(201).json(newPet);
-    } catch (error) {
-      res.status(500).json({ error: 'Erro ao criar um novo pet.' + error });
-    }
-  }
-
-  async function updatePet (req, res){
-    const { name, species, breed, color, weight, sex, age, photo, behavior, neurologicalDisorders, notes } = req.body;
-  
-    try {
-      const pet = await Pet.findOne({ where: { id: req.params.id } });
-  
-      if (pet) {
-        await pet.update({
-          name,
-          species,
-          breed,
-          color,
-          weight,
-          sex,
-          age,
-          photo,
-          behavior,
-          neurologicalDisorders,
-          notes,
-        });
-        res.json(pet);
-      } else {
-        res.status(404).json({ error: 'Pet não encontrado.' });
-      }
-    } catch (error) {
-      res.status(500).json({ error: 'Erro ao atualizar o pet.' + error });
-    }
-  }
-
-  async function deletePet (req, res){
-
-    try {
-      const pet = await Pet.findOne({ where: {id: req.params.id} });
-  
-      if (pet) {
-        await pet.destroy();
-        res.status(204).json(); // Retorna status 204 (sem conteúdo)
-      } else {
-        res.status(404).json({ error: 'Pet não encontrado.' });
-      }
-    } catch (error) {
-      res.status(500).json({ error: 'Erro ao deletar o pet.' + error});
-    }
-  }
-
-  module.exports ={ 
-    helloWorld,
+  module.exports = {
     getAllPets,
     getPetById,
     getPetByName,
     getMostRecentPet,
-    getPetByTutorName,
-    createPet,
-    updatePet,
-    deletePet  
+    getPetByTutorName
   }
--- router/petRoutes.js
+++ router/petRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const controllers = require("../controllers/petControllers")
+const controllers = require("../controllers/petControllers/petExports")
 
 // Rota de teste
 router.get("/", controllers.helloWorld);
