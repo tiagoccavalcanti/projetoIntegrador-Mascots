19e55b50bbfd04bcacebe5342287fe0dc66670a7
TIAGO DO CARMO CAVALCANTI <TIAGO1462@IOT>
2024-11-04 11:49:17 -0300

    working on utils

--- controllers/petControllers/createPet.js
+++ controllers/petControllers/createPet.js
@@ -1,27 +1,32 @@
 const { PersonRegister, Address, Contact, Role, Pet, Patient, Cage, Treatment, Medication, PatientsDailyChart } = require("../../db/models/modelSequelize")
 
-async function createPet (req, res){
-    const { name, species, breed, color, weight, sex, age, photo, behavior, neurologicalDisorders, notes } = req.body;
-    try {
-      const newPet = await Pet.create({
-        name,
-        species,
-        breed,
-        color,
-        weight,
-        sex,
-        age,
-        photo,
-        behavior,
-        neurologicalDisorders,
-        notes
-      });
-      res.status(201).json(newPet);
-    } catch (error) {
-      res.status(500).json({ error: 'Erro ao criar um novo pet.' + error });
-    }
-  };
 
-  module.exports = {
-    createPet
+
+async function createPet(req, res) {
+  const { name, species, breed, color, weight, sex, age, photo, behavior, neurologicalDisorders, notes, cpf } = req.body;
+
+  try {
+    formatarcpf(cpf)
+    const newPet = await Pet.create({
+      name,
+      species,
+      breed,
+      color,
+      weight,
+      sex,
+      age,
+      photo,
+      behavior,
+      neurologicalDisorders,
+      notes
+    });
+
+    res.status(201).json(newPet);
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao criar um novo pet.' + error });
   }
+};
+
+module.exports = {
+  createPet
+}
